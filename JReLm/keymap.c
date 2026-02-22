c7d8de32ff0c465be575eda06916677d6df9a9ee
Ashton Kemerling <ashton.kemerling@protonmail.com>
2026-02-22 22:06:30 +0100

    Update keymap.c
    Typo.

--- JReLm/keymap.c
+++ JReLm/keymap.c
@@ -705,8 +705,6 @@ uint8_t layer_state_set_user(uint8_t state) {
   return state;
 };
 
-cost graphite
-
 const key_override_t dash_key_override = 
     ko_make_basic_with_layers(MOD_MASK_SHIFT, KC_MINUS, KC_QUOTE, 1<<1);
 
